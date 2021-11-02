d6022cbe20fb30d7d5d60f23dc3c1bd96da4f380
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-11-02 10:41:30 +0100

    remove example and wip descriptors

--- zairachem/descriptors/unsupervised.py
+++ zairachem/descriptors/unsupervised.py
@@ -1,3 +1,91 @@
+import joblib
+
+
+class NanFilter():
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class Scaler():
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class Imputer(self):
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class VarianceFilter(self):
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class Pca(self):
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class OptSne(self):
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+class Umap(self):
+
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def transform(self):
+        pass
+
+
+
 class UnsupervisedTransformations(self):
     def __init__(self):
         pass
