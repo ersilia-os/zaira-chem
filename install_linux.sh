6d910f590c5abd701954974aabee65c15ffe9d0c
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-01-28 13:00:30 +0100

    install file wip

--- install_linux.sh
+++ install_linux.sh
@@ -4,16 +4,16 @@ conda init bash
 
 # create zairachem conda environment
 ZAIRACHEM_ENVIRONMENT='zairachem_test'
-#conda create -n $ZAIRACHEM_ENVIRONMENT python=3.7 -y
+conda create -n $ZAIRACHEM_ENVIRONMENT python=3.7 -y
 source $CONDA_PREFIX/etc/profile.d/conda.sh
 conda activate $ZAIRACHEM_ENVIRONMENT
 
 # install ersilia
-python -m pip install -e git+https://github.com/ersilia-os/ersilia.git
-ersilia --help
+#python -m pip install ersilia
+#ersilia --help
 
 # # install isaura
-# python -m pip install git+https://github.com/ersilia-os/isaura.git
+#python -m pip install git+https://github.com/ersilia-os/isaura.git
 
 # # # install zairachem
 # python -m pip install -e .
