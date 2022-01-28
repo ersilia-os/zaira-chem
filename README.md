74b837526616a4bc8ebb72abacf9938f8dfff2fb
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-01-28 12:13:15 +0100

    install script wip

--- README.md
+++ README.md
@@ -1,40 +1,12 @@
-# ZairaChem: out-of-the-box QSAR
+# ZairaChem: Automated ML-based (Q)SAR
 
-**WORK IN PROGRESS**
-
-ZairaChem is the first library of Ersilia's family of tools devoted to providing **out-of-the-box** machine learning solutions for biomedical problems. In this case, we have focused on QSAR models. QSAR models have chemical structures as input and as output they have predicted properties, typically pharmacological properties such as bioactivity against a certain target.
+ZairaChem is the first library of Ersilia's family of tools devoted to providing **out-of-the-box** machine learning solutions for biomedical problems. In this case, we have focused on (Q)SAR models. (Q)SAR models take chemical structures as input and give as output predicted properties, typically pharmacological properties such as bioactivity against a certain target.
 
 ## Installation
 
-### Create a conda environment
-
-```bash
-conda create -n zairachem python=3.7
-conda activate zairachem
+Simply run
 ```
-
-### Install ersilia
-
-```bash
-git clone git@github.com:ersilia-os/ersilia.git
-cd ersilia
-python -m pip install -e .
-```
-
-### Install isaura
-
-```bash
-git clone git@github.com:ersilia-os/isaura.git
-cd isaura
-python -m pip install -e .
-```
-
-### Install zairachem
-
-```bash
-git clone git@github.com:ersilia-os/zaira-chem.git
-cd zaira-chem
-python -m pip install -e .
+bash install_linux.sh
 ```
 
 ## Usage
--- install_linux.sh
+++ install_linux.sh
@@ -1,34 +1,36 @@
 WORKDIR=$PWD
 
+conda init bash
+
 # create zairachem conda environment
 ZAIRACHEM_ENVIRONMENT='zairachem_test'
-conda create -n $ZAIRACHEM_ENVIRONMENT python=3.7
+#conda create -n $ZAIRACHEM_ENVIRONMENT python=3.7 -y
 source $CONDA_PREFIX/etc/profile.d/conda.sh
 conda activate $ZAIRACHEM_ENVIRONMENT
 
 # install ersilia
-python -m python -m pip install git+git@github.com:ersilia-os/ersilia.git
+python -m pip install -e git+https://github.com/ersilia-os/ersilia.git
 ersilia --help
 
-# install isaura
-python -m python -m pip install git+git@github.com:ersilia-os/isaura.git
+# # install isaura
+# python -m pip install git+https://github.com/ersilia-os/isaura.git
 
-# install zairachem
-python -m pip install -e .
+# # # install zairachem
+# python -m pip install -e .
 
-# create extra conda envirnoments
+# # create extra conda envirnoments
 
-# install molmap
-MOLMAP_ENVIRONMENT='molmap'
-conda create -n $MOLMAP_ENVIRONMENT python=3.6 -y
-source $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate $MOLMAP_ENVIRONMENT
-cd zairachem/tools/molmap/bidd-molmap/
-python -m pip install -r requirements.txt
-cd $WORKDIR
+# # install molmap
+# MOLMAP_ENVIRONMENT='molmap'
+# conda create -n $MOLMAP_ENVIRONMENT python=3.6 -y
+# source $CONDA_PREFIX/etc/profile.d/conda.sh
+# conda activate $MOLMAP_ENVIRONMENT
+# cd zairachem/tools/molmap/bidd-molmap/
+# python -m pip install -r requirements.txt
+# cd $WORKDIR
 
-# install mollib
-cd zairachem/tools/mollib/virtual_libraries/
-bash install_linux.sh
-cd $WORKDIR
+# # install mollib
+# cd zairachem/tools/mollib/virtual_libraries/
+# bash install_linux.sh
+# cd $WORKDIR
 
