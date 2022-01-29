3859586fca7fcb03b8aae3a450de8273aef0594d
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-01-29 11:00:32 +0100

    bash install script

--- molmap.sh
+++
@@ -1,8 +0,0 @@
-MOLMAP_ENVIRONMENT='molmap'
-conda create -n $MOLMAP_ENVIRONMENT python=3.6 -y
-source $CONDA_PREFIX/etc/profile.d/conda.sh
-conda activate $MOLMAP_ENVIRONMENT
-conda install -c tmap tmap -y
-cd zairachem/tools/molmap/bidd-molmap/
-python -m pip install -r requirements.txt
-cd $WORKDIR
