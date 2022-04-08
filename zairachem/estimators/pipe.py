efe7468d2d7dff40942985147906760a012c5422
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-04-08 17:37:10 +0200

    added pipeline steps

--- zairachem/estimators/pipe.py
+++ zairachem/estimators/pipe.py
@@ -1,4 +1,5 @@
 from .. import ZairaBase
+from ..utils.pipeline import PipelineStep
 from .from_individual_full_descriptors.pipe import IndividualFullDescriptorPipeline
 from .from_manifolds.pipe import ManifoldPipeline
 from .from_reference_embedding.pipe import ReferenceEmbeddingPipeline
@@ -15,27 +16,42 @@ class EstimatorPipeline(ZairaBase):
             self.path = path
 
     def _individual_estimator_pipeline(self, time_budget_sec):
-        self.logger.debug("Running individual estimator pipeline")
-        p = IndividualFullDescriptorPipeline(path=self.path)
-        p.run(time_budget_sec=time_budget_sec)
+        step = PipelineStep("individual_estimator_pipeline")
+        if not step.is_done():
+            self.logger.debug("Running individual estimator pipeline")
+            p = IndividualFullDescriptorPipeline(path=self.path)
+            p.run(time_budget_sec=time_budget_sec)
+            step.update()
 
     def _manifolds_pipeline(self, time_budget_sec):
-        self.logger.debug("Running manifolds estimator pipeline")
-        p = ManifoldPipeline(path=self.path)
-        p.run(time_budget_sec=time_budget_sec)
+        step = PipelineStep("manifolds_pipeline")
+        if not step.is_done():
+            self.logger.debug("Running manifolds estimator pipeline")
+            p = ManifoldPipeline(path=self.path)
+            p.run(time_budget_sec=time_budget_sec)
+            step.update()
 
     def _reference_pipeline(self, time_budget_sec):
-        self.logger.debug("Reference embedding pipeline")
-        p = ReferenceEmbeddingPipeline(path=self.path)
-        p.run(time_budget_sec=time_budget_sec)
+        step = PipelineStep("reference_pipeline")
+        if not step.is_done():
+            self.logger.debug("Reference embedding pipeline")
+            p = ReferenceEmbeddingPipeline(path=self.path)
+            p.run(time_budget_sec=time_budget_sec)
+            step.update()
 
     def _molmap_pipeline(self, time_budget_sec):
-        self.logger.debug("Molmap estimator pipeline")
-        p = MolMapPipeline(path=self.path)
-        p.run(time_budget_sec=time_budget_sec)
+        step = PipelineStep("molmap_pipeline")
+        if not step.is_done():
+            self.logger.debug("Molmap estimator pipeline")
+            p = MolMapPipeline(path=self.path)
+            p.run(time_budget_sec=time_budget_sec)
+            step.update()
 
     def _simple_evaluation(self):
-        SimpleEvaluator(path=self.path).run()
+        step = PipelineStep("simple_evaluation")
+        if not step.is_done():
+            SimpleEvaluator(path=self.path).run()
+            step.update()
 
     def run(self, time_budget_sec=None):
         self._individual_estimator_pipeline(time_budget_sec)
