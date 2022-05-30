0606a969c4f6c92e0b19dc4fc3796f52db2b3000
miquelduranfrigola <miquelduranfrigola@gmail.com>
2022-05-30 20:05:01 +0200

    added sampler and stoned

---
+++ zairachem/tools/exmol/sampler.py
@@ -0,0 +1,11 @@
+from exmol import run_stoned
+
+
+class StonedSampler(object):
+
+    def __init__(self, max_mutations=2, min_mutations=1):
+        self.max_mutations = max_mutations
+        self.min_mutations = min_mutations
+
+    def sample(self, smiles, n):
+        return run_stoned(smiles, num_samples=n, max_mutations=self.max_mutations, min_mutations=self.min_mutations)
