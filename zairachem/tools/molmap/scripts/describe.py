456b3bbdb6d3e12bf141b734616a7f7766875e58
gemmaturon <gemma@ersilia.io>
2023-07-13 10:07:40 +0200

    bidd-molmap two models

--- zairachem/tools/molmap/scripts/describe.py
+++ zairachem/tools/molmap/scripts/describe.py
@@ -15,10 +15,12 @@ data = pd.read_csv(file_name)
 
 smiles_list = list(data[SMILES_COLUMN])
 
-with ErsiliaModel("bidd-molmap") as mdl:
-    X1 = mdl.descriptors(input=smiles_list, output="numpy")
+with ErsiliaModel("bidd-molmap-desc") as mdl:
+    X1 = mdl.run(input=smiles_list, output="numpy")
     X1 = X1.reshape(X1.shape[0], 37, 37, 1)
-    X2 = mdl.fingerprints(input=smiles_list, output="numpy")
+
+with ErsiliaModel("bidd-molmap-fps") as mdl:
+    X2 = mdl.run(input=smiles_list, output="numpy")
     X2 = X2.reshape(X2.shape[0], 37, 36, 1)
 
 with open(x1_path, "wb") as f:
